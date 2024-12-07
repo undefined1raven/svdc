f879201127e9a2ff08e333ee42cffb13b8fc41ae
undefined1raven <zlat.dominic.francisc@gmail.com>
2024-12-07 16:54:37 +0200

    fix: tree bug fixes
    Signed-off-by: undefined1raven <zlat.dominic.francisc@gmail.com>

---
+++ src/deco/DropdownDeco.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let { classNames = 'size-6', color = '#000000' } = $props();
+</script>
+
+<svg
+	class={classNames}
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke={color}
+>
+	<path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
--- src/framework/Tree.svelte
+++ src/framework/Tree.svelte
@@ -4,6 +4,9 @@
 	import FlyInContainer from '../components/common/FlyInContainer.svelte';
 	import { files } from '../stores/files';
 	import { activeMdContent } from '../stores/activeMdContent';
+	import DropdownDeco from '../deco/DropdownDeco.svelte';
+	import FadeInContainer from '../components/common/FadeInContainer.svelte';
+	import { globalStyle } from '../stores/globalStyle.svelte';
 
 	let currentTree = $state([]);
 
@@ -85,35 +88,57 @@
 			currentTree = newCurrentTree;
 		}
 	}
+
+	function getDropdownRotation(index: number) {
+		const item = currentTree[index];
+		const nextItem = currentTree[index + 1];
+		if (nextItem) {
+			if (nextItem.depth > item.depth) {
+				return '180deg';
+			} else {
+				return '0deg';
+			}
+		}
+	}
 </script>
 
 <div class="h-auto w-full">
 	<div class="flex h-full w-full flex-col gap-1 overflow-x-hidden rounded-md">
 		{#each currentTree as branch, ix}
-			<div class="hover:bg-color40 flex h-12 flex-row text-text">
-				<div
-					style="left: {20 * branch.depth}px;"
-					class="relative flex flex-grow items-center gap-2 rounded-md"
-				>
-					<div class="h-full w-[2px] bg-color"></div>
+			<FadeInContainer duration={80}>
+				<div class="hover:bg-color40 flex h-12 flex-row text-text">
 					<div
-						onclick={() => handleMenuClick(ix)}
-						class="transition-transition linear hover:bg-color40 flex h-full w-auto min-w-[250px] items-center justify-between rounded-md border border-color bg-color20 p-2 transition-all duration-100"
+						style="left: {20 * branch.depth}px;"
+						class="relative flex flex-grow items-center gap-2 rounded-md"
 					>
-						<span>
-							{branch.name}
-						</span>
-						{#if branch.canExtend}
-							<button
-								onclick={() => {
-									handleExpand(ix);
-								}}
-								class="h-full w-12 bg-red-400"
-							></button>
-						{/if}
+						<div class="h-full w-1 bg-color"></div>
+						<div
+							onclick={() => handleMenuClick(ix)}
+							class=" transition-transition linear hover:bg-color40 flex h-full w-auto min-w-[250px] cursor-pointer items-center justify-between rounded-md border border-color bg-color20 p-2 transition-all duration-100"
+						>
+							<span>
+								{branch.name}
+							</span>
+							{#if branch.canExtend}
+								<button
+									onclick={() => {
+										handleExpand(ix);
+									}}
+									class="flex h-full w-16 items-center justify-center pl-8"
+								>
+									<div
+										style="transform: rotate({getDropdownRotation(ix)});"
+										class="linear h-full w-full transition-all duration-100"
+									>
+										<DropdownDeco color={$globalStyle.color} classNames="w-full h-full"
+										></DropdownDeco>
+									</div>
+								</button>
+							{/if}
+						</div>
 					</div>
 				</div>
-			</div>
+			</FadeInContainer>
 		{/each}
 	</div>
 </div>
